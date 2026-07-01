df8eb077d7b88586bf4774e469e2abcd47f913c5
priyanshu655 <patelpriyanshu1209@gmail.com>
2026-07-01 10:38:26 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8080"
+export const ServerUrl  = "https://mern-ai-interview.onrender.com"
 
 function App() {
 
