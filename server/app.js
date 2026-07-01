f9faeb689496c3cbbf477d03b3551014c588140d
priyanshu655 <patelpriyanshu1209@gmail.com>
2026-07-01 10:46:38 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ import { redisRateLimiter } from "./middleware/redisRateLimiter.js";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://mern-ai-interview.onrender.com",
     credentials: true
 }));
 
